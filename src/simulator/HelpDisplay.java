f6168aea4cb5cc9a73340cd93d3f0a8bfb62b24f
Danny Gramowski <dgramows@gmail.com>
2019-02-02 11:24:59 -0500

    cleaned up code

--- src/simulator/HelpDisplay.java
+++ src/simulator/HelpDisplay.java
@@ -2,171 +2,104 @@ package simulator;
 
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
-import com.jme3.app.state.AppState;
-import com.jme3.app.state.AppStateManager;
 import com.jme3.app.state.BaseAppState;
 import com.jme3.asset.AssetManager;
 import com.jme3.font.BitmapFont;
 import com.jme3.font.BitmapText;
 import com.jme3.font.LineWrapMode;
-import com.jme3.font.Rectangle;
 import com.jme3.input.InputManager;
 import com.jme3.input.KeyInput;
-import com.jme3.input.RawInputListener;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
-import com.jme3.input.event.KeyInputEvent;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState.BlendMode;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
-import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
 import com.jme3.scene.Spatial.CullHint;
 import com.jme3.scene.shape.Quad;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import com.jme3.input.InputManager;
-import com.jme3.input.KeyInput;
 
-public class HelpDisplay extends BaseAppState  {
-	
-	private boolean initialized = false;
-    private boolean visible = false;
-    private SimpleApplication app;
-    private AssetManager assetManager;
-    private InputManager inputManager;
-    private ViewPort guiViewPort;
-    private Node guiNode;
-    private Node consoleBaseNode;
-    private BlendMode materialBlendMode = BlendMode.Alpha;
-    private Geometry helpQuad;
-    private String enqBitmapFontAssetName = "Interface/Fonts/Default.fnt"; // this variable is reset to null after being consumed by applyViewportChange().
-    private BitmapText scrollingBitmapText;
+public class HelpDisplay extends BaseAppState {
+	private ViewPort guiViewPort;
+	private Node consoleBaseNode;
 	private boolean helpVisible = false;
-  	
+
 	protected void initialize(Application _app) {
-		
-		initialized = true;
-        app = (SimpleApplication) _app;
-        assetManager = app.getAssetManager();
-        inputManager = app.getInputManager();
-        guiViewPort = app.getGuiViewPort();
-        guiNode = app.getGuiNode();
-        visible = true;
-        InputManager manager = app.getInputManager();
+		SimpleApplication app;
+		app = (SimpleApplication) _app;
+		Node guiNode = app.getGuiNode();
+		guiViewPort = app.getGuiViewPort();
+		InputManager manager = app.getInputManager();
+		manager.addListener(keyListener, "h");
 		manager.addMapping("h", new KeyTrigger(KeyInput.KEY_H));
-		manager.addListener(keyListener,"h");
+		BlendMode materialBlendMode = BlendMode.Alpha;
+		AssetManager assetManager = app.getAssetManager();
+		String enqBitmapFontAssetName = "Interface/Fonts/Default.fnt";
+		consoleBaseNode = new Node("Console");
 
-        if (consoleBaseNode == null) {
-              consoleBaseNode = new Node("Console");
-              
-              Material darkGrayMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-              darkGrayMat.setColor("Color", new ColorRGBA(0, 0, 10, 0.3f));
-              darkGrayMat.getAdditionalRenderState().setBlendMode(materialBlendMode);
+		Material darkGrayMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		darkGrayMat.setColor("Color", new ColorRGBA(0, 0, 10, 0.3f));
+		darkGrayMat.getAdditionalRenderState().setBlendMode(materialBlendMode);
 
-              helpQuad = new Geometry("helpQuad");
-              helpQuad.setMaterial(darkGrayMat);
-              
-              consoleBaseNode.attachChild(helpQuad);
-              consoleBaseNode.setCullHint(CullHint.Always);
-              
-              guiNode.attachChild(consoleBaseNode); 
-              applyViewPortChangeNotThreadSafe();
-        }
-	} 
-    private void applyViewPortChangeNotThreadSafe() {
-    	int viewPortHeight = guiViewPort.getCamera().getHeight();
-        int viewPortWidth = guiViewPort.getCamera().getWidth();
-    	 float helpQuadHeight = (viewPortHeight/2);
-         float helpQuadWidth = (viewPortWidth/5);
-         float helpQuadStartX = guiViewPort.getCamera().getWidth() - helpQuadWidth;
-         float helpQuadStartY = guiViewPort.getCamera().getHeight() - helpQuadHeight;
+		Geometry helpQuad = new Geometry("helpQuad");
+		helpQuad.setMaterial(darkGrayMat);
 
-            BitmapFont guiFont = assetManager.loadFont(enqBitmapFontAssetName); //Interface/Fonts/Default.fnt
-            enqBitmapFontAssetName = null;
-            scrollingBitmapText = new BitmapText(guiFont, false);
-            scrollingBitmapText.setName("scrollingBitmapText");
-            scrollingBitmapText.setLineWrapMode(LineWrapMode.Word);
-            scrollingBitmapText.setColor("Color", new ColorRGBA(255,0,0,1f));
-            scrollingBitmapText.setText("left move forward = q\n "
-               + "left move back = a\n"
-               + "right move forward = e\n"
-               + "right move back = d\n" 
-               + "pause  = p\n"
-               + "resume = space\n"
-               + "(you must resume after you start\n"
-               + "before the robot will move)\n"
-               + "reset = r\n"
-               + "help = h\n"
-               + "move camera = left mouse\n"
-               + "zoom = middle mouse\n"
-               + "pick up hatch = w\n"
-               + "drop hatch = s\n"
-               + "debug window = t");
-            scrollingBitmapText.setLocalTranslation(helpQuadStartX + 100,
-            		helpQuadStartY + helpQuadHeight - 100,0);
-            consoleBaseNode.attachChild(scrollingBitmapText);	 
-   
-       helpQuad.setMesh(new Quad(helpQuadWidth, helpQuadHeight));
-       helpQuad.setLocalTranslation(helpQuadStartX, helpQuadHeight, 0);
-    }
-    public void setUseAlphaOnConsoleGeoms(boolean useAlpha) {
-        if (useAlpha) {
-              materialBlendMode = BlendMode.Alpha;
-        } else {
-              materialBlendMode = BlendMode.Off;
-      if (this.isInitialized()) {
-          app.enqueue(new Callable<Void>() {
-                @Override
-                public Void call() throws Exception {
-                      helpQuad.getMaterial().getAdditionalRenderState().setBlendMode(materialBlendMode);
-                      return null;
-            }
-	      });
-	    }
-      }
-    }		
-	private final ActionListener keyListener = new ActionListener() {		
-		 public void onAction(String name, boolean keyPressed, float tpf) {
+		consoleBaseNode.attachChild(helpQuad);
+		consoleBaseNode.setCullHint(CullHint.Always);
 
-				if (name.equals("h") && keyPressed) {
-					if(helpVisible == false) {
-						consoleBaseNode.setCullHint(CullHint.Dynamic);
-						helpVisible = true;
-					} else {
-						consoleBaseNode.setCullHint(CullHint.Always);
-						helpVisible = false;
-					}
+		guiNode.attachChild(consoleBaseNode);
+
+		int viewPortHeight = guiViewPort.getCamera().getHeight();
+		int viewPortWidth = guiViewPort.getCamera().getWidth();
+		float helpQuadHeight = (viewPortHeight / 2);
+		float helpQuadWidth = (viewPortWidth / 5);
+		float helpQuadStartX = guiViewPort.getCamera().getWidth() - helpQuadWidth;
+		float helpQuadStartY = guiViewPort.getCamera().getHeight() - helpQuadHeight;
+
+		BitmapFont guiFont = assetManager.loadFont(enqBitmapFontAssetName); // Interface/Fonts/Default.fnt
+		BitmapText scrollingBitmapText = new BitmapText(guiFont, false);
+		scrollingBitmapText.setName("scrollingBitmapText");
+		scrollingBitmapText.setColor("Color", new ColorRGBA(255, 0, 0, 1f));
+		scrollingBitmapText.setText("left move forward = q\n " + "left move back = a\n" + "right move forward = e\n"
+				+ "right move back = d\n" + "pause  = p\n" + "resume = space\n" + "(you must resume after you start\n"
+				+ "before the robot will move)\n" + "reset = r\n" + "help = h\n" + "move camera = left mouse\n"
+				+ "zoom = middle mouse\n" + "pick up hatch = w\n" + "drop hatch = s\n" + "debug window = t");
+		scrollingBitmapText.setLocalTranslation(helpQuadStartX + 100, helpQuadStartY + helpQuadHeight - 100, 0);
+		consoleBaseNode.attachChild(scrollingBitmapText);
+
+		helpQuad.setMesh(new Quad(helpQuadWidth, helpQuadHeight));
+		helpQuad.setLocalTranslation(helpQuadStartX, helpQuadHeight, 0);
+	}
+
+	private final ActionListener keyListener = new ActionListener() {
+		public void onAction(String name, boolean keyPressed, float tpf) {
+
+			if (name.equals("h") && keyPressed) {
+				if (helpVisible == false) {
+					consoleBaseNode.setCullHint(CullHint.Dynamic);
+					helpVisible = true;
+				} else {
+					consoleBaseNode.setCullHint(CullHint.Always);
+					helpVisible = false;
 				}
 			}
+		}
 
-	 	};
+	};
 
 	@Override
 	protected void cleanup(Application arg0) {
 		// TODO Auto-generated method stub
-			      }
+	}
 
 	@Override
 	protected void onDisable() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	protected void onEnable() {
 		// TODO Auto-generated method stub
-		
 	}
 }
-	
-	
-	
--- src/simulator/infoDisplay.java
+++ src/simulator/infoDisplay.java
@@ -2,148 +2,95 @@ package simulator;
 
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
-import com.jme3.app.state.AppState;
-import com.jme3.app.state.AppStateManager;
 import com.jme3.app.state.BaseAppState;
 import com.jme3.asset.AssetManager;
 import com.jme3.font.BitmapFont;
 import com.jme3.font.BitmapText;
-import com.jme3.font.LineWrapMode;
-import com.jme3.font.Rectangle;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState.BlendMode;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Quad;
 
-import de.lessvoid.nifty.elements.Element;
-import strongdk.jme.appstate.console.ConsoleAppState;
+public class infoDisplay extends BaseAppState {
+	private BitmapText scrollingBitmapText;
+	private Robot robot;
+	private Vector3f lastPosition;
+	private float timePast;
+	private float timeTotal;
+	private float speed;
 
-import java.io.PrintWriter;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.Callable;
-
-public class infoDisplay extends BaseAppState  {
-	
-	private boolean initialized = false;
-    private SimpleApplication app;
-    private AssetManager assetManager;
-    private ViewPort guiViewPort;
-    private Node guiNode;
-    private Node consoleBaseNode;
-    private BlendMode materialBlendMode = BlendMode.Alpha;
-    private Geometry infoQuad;
-    private String enqBitmapFontAssetName = "Interface/Fonts/Default.fnt"; 
-    // this variable is reset to null after being consumed by applyViewportChange().
-    private BitmapText scrollingBitmapText;
-    Robot robot;
-    Vector3f lastPosition;
-    float timePast;
-    float timeTotal;
-    float speed;
-;	
 	protected void initialize(Application _app) {
-		initialized = true;
-        app = (SimpleApplication) _app;
-        assetManager = app.getAssetManager();
-        guiViewPort = app.getGuiViewPort();
-        robot = app.getStateManager().getState(Robot.class);
-        guiNode = app.getGuiNode();
-        if (consoleBaseNode == null) {
-              consoleBaseNode = new Node("Console");
-              
-              Material darkGrayMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-              darkGrayMat.setColor("Color", new ColorRGBA(0, 0, 2, .3f));
-              darkGrayMat.getAdditionalRenderState().setBlendMode(materialBlendMode);
+		String enqBitmapFontAssetName = "Interface/Fonts/Default.fnt";
+		SimpleApplication app = (SimpleApplication) _app;
+		robot = app.getStateManager().getState(Robot.class);
+		AssetManager assetManager = app.getAssetManager();
+		ViewPort guiViewPort = app.getGuiViewPort();
+		BlendMode materialBlendMode = BlendMode.Alpha;
+		Node guiNode = app.getGuiNode();
 
-              infoQuad = new Geometry("infoQuad");
-              infoQuad.setMaterial(darkGrayMat);
+		Node consoleBaseNode = new Node("Console");
 
-              consoleBaseNode.attachChild(infoQuad);
-              guiNode.attachChild(consoleBaseNode);
-              
-              applyViewPortChangeNotThreadSafe();
-        }
-	} 
-    private void applyViewPortChangeNotThreadSafe() {
-    	int viewPortHeight = guiViewPort.getCamera().getHeight();
-        int viewPortWidth = guiViewPort.getCamera().getWidth();
-        float infoQuadHeight = (viewPortHeight/4);
-        float infoQuadWidth = (viewPortWidth/7);
-    
-            BitmapFont guiFont = assetManager.loadFont(enqBitmapFontAssetName); //Interface/Fonts/Default.fnt
-            enqBitmapFontAssetName = null;
-            scrollingBitmapText = new BitmapText(guiFont, false);
-            scrollingBitmapText.setName("scrollingBitmapText");
-            scrollingBitmapText.setLineWrapMode(LineWrapMode.Word);
-            scrollingBitmapText.setColor("Color", new ColorRGBA(255,0,0,1f));
-            scrollingBitmapText.setLocalTranslation(infoQuadWidth/3.9f,infoQuadHeight/2,0);
-            consoleBaseNode.attachChild(scrollingBitmapText);
-     
-       infoQuad.setMesh(new Quad(infoQuadWidth, infoQuadHeight));
-       infoQuad.setLocalTranslation(0, 0, 0);
-      
-    }
-    public void setUseAlphaOnConsoleGeoms(boolean useAlpha) {
-        if (useAlpha) {
-              materialBlendMode = BlendMode.Alpha;
-        } else {
-              materialBlendMode = BlendMode.Off;
-        }
+		Material darkGrayMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		darkGrayMat.setColor("Color", new ColorRGBA(0, 0, 2, .3f));
+		darkGrayMat.getAdditionalRenderState().setBlendMode(materialBlendMode);
 
-        if (this.isInitialized()) {
-              app.enqueue(new Callable<Void>() {
-                    @Override
-                    public Void call() throws Exception {
-                          infoQuad.getMaterial().getAdditionalRenderState().setBlendMode(materialBlendMode);
-                          return null;
-                    }
-              });
-        }
-  }
-    public void update(float tpf) {    
-    	Vector3f newPosition = robot.getRobotBase().getWorldTranslation();
-    	timePast += tpf;
-    	timeTotal += tpf;
-    	if(timePast > 0.5)
-		{
+		Geometry infoQuad = new Geometry("infoQuad");
+		infoQuad.setMaterial(darkGrayMat);
+
+		consoleBaseNode.attachChild(infoQuad);
+		guiNode.attachChild(consoleBaseNode);
+
+		int viewPortHeight = guiViewPort.getCamera().getHeight();
+		int viewPortWidth = guiViewPort.getCamera().getWidth();
+		float infoQuadHeight = (viewPortHeight / 4);
+		float infoQuadWidth = (viewPortWidth / 7);
+
+		BitmapFont guiFont = assetManager.loadFont(enqBitmapFontAssetName);
+		scrollingBitmapText = new BitmapText(guiFont, false);
+		scrollingBitmapText.setName("scrollingBitmapText");
+		scrollingBitmapText.setColor("Color", new ColorRGBA(255, 0, 0, 1f));
+		scrollingBitmapText.setLocalTranslation(infoQuadWidth / 3.9f, infoQuadHeight / 2, 0);
+		consoleBaseNode.attachChild(scrollingBitmapText);
+		infoQuad.setMesh(new Quad(infoQuadWidth, infoQuadHeight));
+	}
+
+	public void update(float tpf) {
+		Vector3f newPosition = robot.getRobotBase().getWorldTranslation();
+		timePast += tpf;
+		timeTotal += tpf;
+		if (timePast > 0.5) {
 			if (lastPosition != null) {
-				float distance = newPosition.distance(lastPosition);			
-				speed = distance/timePast;
+				float distance = newPosition.distance(lastPosition);
+				speed = distance / timePast;
 			}
 			lastPosition = newPosition.clone();
 			timePast = 0;
 		}
 
-    	String displayText = String.format("the total time is %.2f", timeTotal);
-    	displayText += String.format("\n   your speed is %.2f", speed);
-
-    	scrollingBitmapText.setText(displayText);
+		String displayText = String.format("the total time is %.2f", timeTotal);
+		displayText += String.format("\n  your speed is %.2f", speed);
+		scrollingBitmapText.setText(displayText);
 	}
+
 	@Override
 	protected void cleanup(Application arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void onDisable() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void onEnable() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
